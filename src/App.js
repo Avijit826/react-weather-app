f5f9d299e098fc925a0405d1c55c2f14531a65c1
Avijit Das <avijitdas826@gmail.com>
2023-02-19 23:54:07 +0530

    Implement Weather Api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react"
+// require("dotenv").config()
 import "./App.css"
-
+const apiUrl = process.env.REACT_APP_API_URL
+const apiKey = process.env.REACT_APP_API_KEY
 function App() {
-  return (
+  const [details, setDetails] = useState(null)
+  // const [weather, setWeather] = useState({})
+  const getData = async () => {
+    const response = await fetch(
+      `${apiUrl}?q=kolkata&units=metric&appid=${apiKey}`
+    )
+    const data = await response.json()
+    setDetails(data)
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return details ? (
     <div className="App">
-      <p>hii</p>
+      <img
+        src={`https://openweathermap.org/img/wn/${details.weather[0].icon}@4x.png`}
+        alt="icon"
+      />
+      <p>hii {details.name}</p>
+      <p>hii {details.main.temp}</p>
+      {/* {console.log(apiUrl)} */}
     </div>
+  ) : (
+    <div>No Data</div>
   )
 }
 
